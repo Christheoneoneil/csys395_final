35ef80297dd18d3b7e47551c2223eafbc6e90755
Chris <chrismoneil99@gmail.com>
2023-02-08 15:17:29 -0500

    Found number of common heros acorss data sets

--- data_munging.py
+++ data_munging.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import os
 import itertools
+from functools import reduce
 
 def read_data(file_list: list) -> list:
     """
@@ -35,8 +36,12 @@ def prelim_analysis(df_list: list) -> None:
     df_list = [df.drop(["Unnamed: 0"], axis=1) if "Unnamed: 0" in  list(df.columns) else df for df in df_list ]
     colnames = [list(df.columns) for df in df_list]
     flatten_cols = [subcols  for cols in colnames for subcols in cols]
+    
     hero_col_names = [col  for col in flatten_cols if "hero" in col.lower() or "name" in col.lower()]
-    
-    
+    heros = [pd.DataFrame(df[hero_col]).rename(columns={hero_col:"name"}) 
+            for df,hero_col in zip(df_list, hero_col_names)]
+    common_heros = reduce(lambda df1,df2: pd.merge(df1,df2,on='name'), heros)
+    print(len(common_heros))
+
 unmerged_dfs = read_data(os.listdir("data/"))
 prelim_analysis(unmerged_dfs)
