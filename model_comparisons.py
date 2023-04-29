8f8c117cf7632c035c61b81270bd8b302c0a6b36
Chris <chrismoneil99@gmail.com>
2023-04-29 14:52:30 -0400

    Explicitly defined args

--- model_comparisons.py
+++ model_comparisons.py
@@ -26,20 +26,20 @@ def metrics(mod, X_t: pd.DataFrame, y_t: pd.DataFrame, y_train: pd.DataFrame)->N
     from sklearn.metrics import balanced_accuracy_score
     from sklearn.metrics import RocCurveDisplay
     from sklearn.preprocessing import LabelBinarizer
-
+    y_t = y_t.to_list()
     preds = mod.predict(X_t)
     probs = mod.predict_proba(X_t)
-    print(classification_report(preds, y_t)) 
-    jacc = jaccard_score(y_t, preds, average = 'macro')
+    print(classification_report(y_pred=preds, y_true=y_t)) 
+    jacc = jaccard_score(y_true=y_t, y_pred=preds, average = 'macro')
     print('The jaccard score is ' + str(jacc))
-    balanced_accuracy = balanced_accuracy_score(y_t, preds)
+    balanced_accuracy = balanced_accuracy_score(y_true=y_t, y_pred=preds)
     print('The accuracy is ', balanced_accuracy)
-    roc = roc_auc_score(y_t, probs, multi_class = 'ovr')
+    roc = roc_auc_score(y_true=y_t, y_score=probs, multi_class = 'ovr')
     print('The ROC AUC is ' + str(roc))
     # Plot confusion matrix
     fig, ax = plt.subplots(figsize=(8, 5))
     cmp = ConfusionMatrixDisplay(
-        confusion_matrix(y_t, preds),
+        confusion_matrix(y_true=y_t, y_pred=preds),
         display_labels=["bad","good", "neutral"]
     )
     
@@ -80,47 +80,51 @@ rf_model, X_test, y_test, y_train= classifier_pipeline(unengineered_dat, "alignm
                                                cv_grid=param_grid, oversampler=SMOTE())
 
 # print(rf_model)
+
+
 metrics(mod=rf_model, X_t=X_test, y_t=y_test, y_train = y_train) 
-# -
 
-from sklearn.linear_model import LogisticRegression
-lr_classifier = LogisticRegression()
-lr_param_grid = {
-                    "class__penalty": ["l2", "none"],
-                    "class__max_iter": np.arange(100, 10000, 100),
-                    "class__multi_class": ["ovr","auto"],
-                    'class__solver' : ['liblinear','sag', 'saga']
-                }
-lr_model, X_test, y_test, y_train = classifier_pipeline(unengineered_dat, "alignment", classifier=lr_classifier,
-                                               cv_grid=lr_param_grid, oversampler=SMOTE())
-# print(lr_model)
-metrics(mod=lr_model, X_t=X_test, y_t=y_test, y_train = y_train)
+# # -
 
-from sklearn.ensemble import ExtraTreesClassifier
-et_classifier = ExtraTreesClassifier()
-et_param_grid = {
-                    "class__n_estimators": np.arange(50,500,5), 
-                    'class__max_features' : ['log2','sqrt','auto'],
-                    'class__criterion' : ['gini', 'entropy'],
-                    'class__max_depth': np.arange(1,20)
-                }
-et_model, X_test, y_test, y_train = classifier_pipeline(unengineered_dat, "alignment", classifier=et_classifier,
-                                               cv_grid=et_param_grid, oversampler=SMOTE())
-# print(et_model)
-metrics(mod = et_model, X_t=X_test, y_t=y_test, y_train = y_train)
+# from sklearn.linear_model import LogisticRegression
+# lr_classifier = LogisticRegression()
+# lr_param_grid = {
+#                     "class__penalty": ["l2", "none"],
+#                     "class__max_iter": np.arange(100, 10000, 100),
+#                     "class__multi_class": ["ovr","auto"],
+#                     'class__solver' : ['liblinear','sag', 'saga']
+#                 }
+# lr_model, X_test, y_test, y_train = classifier_pipeline(unengineered_dat, "alignment", classifier=lr_classifier,
+#                                                cv_grid=lr_param_grid, oversampler=SMOTE())
+# # print(lr_model)
+# metrics(mod=lr_model, X_t=X_test, y_t=y_test, y_train = y_train)
 
-from sklearn.neural_network import MLPClassifier
-mlp_classifier = MLPClassifier()
-mlp_param_grid = {
-            'class__hidden_layer_sizes': [(100,2), (100,3), (100,4), (100,5), (100,6)],
-            'class__activation' : ['identity', 'logistic', 'tanh', 'relu'],
-            'class__solver' : ['sgd','adam'],
-            'class__learning_rate': ['constant', 'invscaling', 'adaptive'],
-            'class__max_iter' : np.arange(200,1000,50),
-             }
-mlp_model, X_test, y_test, y_train = classifier_pipeline(unengineered_dat, "alignment", classifier=mlp_classifier,
-                                                cv_grid=mlp_param_grid, oversampler=SMOTE())
+# from sklearn.ensemble import ExtraTreesClassifier
+# et_classifier = ExtraTreesClassifier()
+# et_param_grid = {
+#                     "class__n_estimators": np.arange(50,500,5), 
+#                     'class__max_features' : ['log2','sqrt','auto'],
+#                     'class__criterion' : ['gini', 'entropy'],
+#                     'class__max_depth': np.arange(1,20)
+#                 }
+# et_model, X_test, y_test, y_train = classifier_pipeline(unengineered_dat, "alignment", classifier=et_classifier,
+#                                                cv_grid=et_param_grid, oversampler=SMOTE())
+# # print(et_model)
+# metrics(mod = et_model, X_t=X_test, y_t=y_test, y_train = y_train)
+
+# from sklearn.neural_network import MLPClassifier
+# mlp_classifier = MLPClassifier()
+# mlp_param_grid = {
+#             'class__hidden_layer_sizes': [(100,2), (100,3), (100,4), (100,5), (100,6)],
+#             'class__activation' : ['identity', 'logistic', 'tanh', 'relu'],
+#             'class__solver' : ['sgd','adam'],
+#             'class__learning_rate': ['constant', 'invscaling', 'adaptive'],
+#             'class__max_iter' : np.arange(200,1000,50),
+#              }
+# mlp_model, X_test, y_test, y_train = classifier_pipeline(unengineered_dat, "alignment", classifier=mlp_classifier,
+#                                                 cv_grid=mlp_param_grid, oversampler=SMOTE())
+
 # print(mlp_model)
-metrics(mod = mlp_model, X_t=X_test, y_t=y_test, y_train = y_train)
+# metrics(mod = mlp_model, X_t=X_test, y_t=y_test, y_train = y_train)
 
 
